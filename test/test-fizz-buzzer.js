82e135dbcab6d0aafb05a8a89e420784700e8dc6
John Joo <john@Johns-iMac.local>
2018-05-17 14:54:08 -0700

    fixed tests

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -5,34 +5,37 @@ const fizzBuzzer = require('../fizzBuzzer').default;
 describe('fizzBuzzer', function() {
 
   // test the normal case
-  it('should return the correct string', function() {
-    // range of normal inputs, including
-    // notable cases like negative answers
-    const normalCases = [
-      {a: 5, b: 3, expected: 'fizz-buzz'},
-      {a: 1, b: 5, expected: 'buzz'},
-      {a: 3, b: 1, expected: 'fizz'}
-    ];
-    // for each set of inputs (a, b), `fizzBuzzer` should
-    // produce the expected value
-    normalCases.forEach(function(input) {
-      const answer = fizzBuzzer(input.a, input.b);
-      expect(answer).to.equal(input.expected);
+  it('should return "fizz-buzz" for multiples of 15', function() { 
+    [15, 30, 45].forEach(function(input) {
+      expect(fizzBuzzer(input)).to.equal('fizz-buzz'); 
+    });
+  }); 
+
+  it('should return "buzz" for multiples of 5', function() {
+    [5, 10, 15].forEach(function(input){
+      expect(fizzBuzzer(input)).to.equal('buzz'); 
     });
   });
 
-  it('should raise error if args not numbers', function() {
-    // range of bad inputs where not both are numbers
-    const badInputs = [
-      ['a', 1],
-      ['1', 2],
-      [2, false]
-    ];
-    // prove that an error is raised for bad inputs
-    badInputs.forEach(function(input) {
-      expect(function() {
-        adder(input[0], input[1]);
-      }).to.throw(Error);
+  it('should return "fizz" for multiples of 3', function() {
+    [3, 6, 9].forEach(function(input) {
+      expect(fizzBuzzer(input)).to.equal('buzz'); 
+    });
+  })
+
+  it ('should return number if not multiples of 3 or 5', function() { 
+    [1, 4, 16].forEach(function(input) { 
+      expect(fizzBuzzer(input)).to.equal(input); 
     });
   });
-});
+
+  it('should raise error if input isn\'t a number', function() {
+    const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]]; 
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzBuzzer(input); 
+      }).to.throw(Error); 
+    });
+  }); 
+  
+}); 
